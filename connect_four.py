31fa139606e83fe552d88a595acc5ee9e115053a
Arden <arden@xins-air-2.myfiosgateway.com>
2023-12-25 10:15:05 -0500

    address comments

--- connect_four.py
+++ connect_four.py
@@ -15,15 +15,8 @@ def set_up():
         p1 = str(input("Player X's name is:  "))
         p2 = str(input("Player O's name is:  "))
     return [p1,p2]
-players = set_up()
-p1 = players[0]
-p2 = players[1]
-n_rows = 6
-n_columns = 7
-n_inaline = 4       # 4 because you need to connect 4 in a line to win
-grid = []
-for i in range(n_rows):
-    grid.append(['.']*n_columns)
+
+
 def gravity(grid):
     '''
     gravity(list) -> list
@@ -31,6 +24,8 @@ def gravity(grid):
     but only brings O's or X's in the air down by one,
     not down the whole column.
     '''
+    n_rows = len(grid)
+    n_columns = len(grid[0])
     new_grid = []
     for i in range(n_rows):
         new_grid.append(['.']*n_columns)
@@ -45,86 +40,132 @@ def gravity(grid):
                 else:
                     new_grid[row][column] = grid[row][column]                    
     return new_grid
-def win(grid):
+def win(grid,connect_n):
     '''
     This function takes in 
-    win(list) -> 'X' or 'O' or ''
+    win(list, int) -> 'X' or 'O' or ''
     The function tells you whether or not someone has won
     and who won.
     '''
     # check if anybody won horizantaly
     for row in range(n_rows):
-        for column in range(n_columns-n_inaline+1):
+        for column in range(n_columns-connect_n+1):
             equal = 0
-            for place in range(n_inaline):
+            for place in range(connect_n):
                 if grid[row][column] !='.':
                     if grid[row][column]==grid[row][column+place]:
                         equal += 1
-            if equal == n_inaline:
+            if equal == connect_n:
                 return grid[row][column]
     # check if anybody won verticaly
-    for row in range(n_rows-n_inaline+1):
+    for row in range(n_rows-connect_n+1):
         for column in range(n_columns):
             equal = 0
-            for place in range(n_inaline):
+            for place in range(connect_n):
                 if grid[row][column] !='.':
                     if grid[row][column]==grid[row+place][column]:
                         equal += 1
-            if equal == n_inaline:
+            if equal == connect_n:
                 return grid[row][column]
     # diagonal one way
-    for row in range(n_rows-n_inaline+1):
-        for column in range(n_columns-n_inaline+1):
+    for row in range(n_rows-connect_n+1):
+        for column in range(n_columns-connect_n+1):
             equal = 0
-            for place in range(n_inaline):
+            for place in range(connect_n):
                 if grid[row][column] != '.':
                     if grid[row][column] == grid[row+place][column+place]:
                         equal += 1
-            if equal == n_inaline:
+            if equal == connect_n:
                 return grid[row][column]
     # diagonal the other way
     for row in range(3):
-        for column in range(n_columns-n_inaline,n_columns):
+        for column in range(n_columns-connect_n,n_columns):
             equal = 0
-            for place in range(n_inaline):
+            for place in range(connect_n):
                 if grid[row][column] != '.':
                     if grid[row][column] == grid[row+place][column-place]:
                         equal += 1
-            if equal == n_inaline:
+            if equal == connect_n:
                 return grid[row][column]
-    return ' '
-# The below is the actual game code, not the functions
-player_turn = 0
-while win(grid) == ' ':
-    print("1 2 3 4 5 6 7")
-    for row in range(6):
+    return False
+
+
+def get_valid(input_str, columns, player):
+    '''
+    get_valid(str) -> int
+    This function makes sure the output will be a valid column to place in.
+    '''
+    is_not_digit = True
+    word = input_str
+    while is_not_digit:
+            if word.isdigit() == True:
+                col = int(word)
+                if col <= columns and col >= 1:
+                    is_not_digit = False    
+                else:
+                    word = input("Please input a number within the range. What column do you want to play, "+player+"?  ")
+            else:
+                word = input("Please input a number within the range. What column do you want to play, "+player+"?  ")             
+    return int(word)
+
+
+def draw_grid(rows, columns, grid):
+    '''
+    draw_grid(int,int,list) -> None
+    draws the grid with rows and columns
+    '''
+    line = ''
+    for column in range(columns):
+        line += str(column+1)+' '
+    print(line)
+    for row in range(n_rows):
         line = ''
-        for column in range(7):
+        for column in range(columns):
             line += grid[row][column]+' '
         print(line)
-    if player_turn%2 == 0:
-        X_word = int(input((p1+" You're X, what column do you want to play in(enter in a number, or else...)?  ")))
-        while grid[0][X_word-1] != '.':
-            X_word = int(input(("Please choose a different column. What column do you want to play, "+p1+"?  ")))
-        grid[0][X_word-1] = "X"
-        grid = gravity(gravity(gravity(gravity(gravity(grid)))))
-    else:
-        O_word = int(input((p2+" You're O, what column do you want to play in(enter in a number, or else...)?  "  )))
-        while grid[0][O_word-1] != '.':
-            O_word = int(input(("Please choose a different column. What column do you want to play, "+p2+"?  "  )))
-        grid[0][O_word-1] = "O"
-        grid = gravity(gravity(gravity(gravity(gravity(grid)))))
-    player_turn += 1
-print("1 2 3 4 5 6 7")
-for row in range(6):
-    line = ''
-    for column in range(7):
-        line += grid[row][column]+' '
-    print(line)
-if win(grid) == 'X':
-    print("Congratulations "+p1+", you won!")
-if win(grid) == 'O':
-    print("Congratulations "+p2+", you won!")
+
+
+
+if __name__ == "__main__":
+    # set up variables
+    players = set_up()
+    p1 = players[0]
+    p2 = players[1]
+    n_rows = 6
+    n_columns = 7
+    # 4 because you need to connect 4 in a line to win
+    n_connect = 4      
+    grid = []
+    # initialize the grid
+    for i in range(n_rows):
+        grid.append(['.']*n_columns)
+
+    player_turn = 0
+    while not win(grid, n_connect):
+        # draws the grid    
+        draw_grid(n_rows,n_columns,grid)
+        # if it's X's turn
+        if player_turn%2 == 0:
+            X_col = input(p1+" You're X, what column do you want to play in?  ")
+            X_col = get_valid(X_col,n_columns,p1)
+            grid[0][X_col-1] = "X"
+        # if it's O's turn 
+        else:
+            O_col = input(p2+" You're O, what column do you want to play in?  ")
+            O_col = get_valid(O_col,n_columns,p2)
+            grid[0][O_col-1] = "O"
+
+        # updates the grid
+        for i in range(n_columns-1):
+            grid = gravity(grid)
+        player_turn += 1
+
+    # who wins??????
+    draw_grid(n_rows,n_columns,grid)
+    if win(grid, n_connect) == 'X':
+        print("Congratulations "+p1+", you won!")
+    if win(grid, n_connect) == 'O':
+        print("Congratulations "+p2+", you won!")
 
 
     
